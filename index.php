f73b7addb0f86e27e2e60c3004168cdb0cd653c1
PROSSYNABBONA <prossienabbona20@gmail.com>
2025-10-06 15:37:31 +0300

    Initial commit: portfolio + Dockerfilenew new ne w another

--- index.php
+++ index.php
@@ -35,6 +35,43 @@ if ($path === '/send_email_simple_render.php' || strpos($path, 'send_email_simpl
     include 'test_endpoint.php';
 } elseif ($path === '/view_logs.php' || strpos($path, 'view_logs.php') !== false) {
     include 'view_logs.php';
+} elseif (strpos($path, '/videos/') === 0) {
+    // Serve MP4 videos with correct headers (and byte-range support)
+    $videoFile = __DIR__ . urldecode($path);
+    if (is_file($videoFile)) {
+        $size = filesize($videoFile);
+        $start = 0;
+        $length = $size;
+        $end = $size - 1;
+        header('Content-Type: video/mp4');
+        header('Accept-Ranges: bytes');
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            // bytes=START-END
+            if (preg_match('/bytes=([0-9]*)-([0-9]*)/', $_SERVER['HTTP_RANGE'], $m)) {
+                if ($m[1] !== '') { $start = (int)$m[1]; }
+                if ($m[2] !== '') { $end = (int)$m[2]; }
+                if ($end >= $size) { $end = $size - 1; }
+                if ($start > $end) { $start = 0; }
+                $length = $end - $start + 1;
+                header("HTTP/1.1 206 Partial Content");
+                header("Content-Range: bytes $start-$end/$size");
+            }
+        }
+        header("Content-Length: $length");
+        $chunk = 8192;
+        $fp = fopen($videoFile, 'rb');
+        if ($start > 0) { fseek($fp, $start); }
+        $bytesSent = 0;
+        while (!feof($fp) && $bytesSent < $length) {
+            $read = ($length - $bytesSent) > $chunk ? $chunk : ($length - $bytesSent);
+            $buffer = fread($fp, $read);
+            echo $buffer;
+            flush();
+            $bytesSent += strlen($buffer);
+        }
+        fclose($fp);
+        exit;
+    }
 } else {
     // Serve the main website
     header('Content-Type: text/html; charset=UTF-8');
